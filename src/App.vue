e233cc31eb30ed0bde1a2d98fd1789b45679d066
teakatara <teakatara@users.noreply.github.com>
2026-07-10 08:21:03 +0000

    名刺カードの仮実装

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import BusinessCard from "./components/BusinessCard.vue";
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
+  <BusinessCard
+    businessCardFrontFaceImageSrc="/src/assets/名刺サイズの画像.png"
+    businessCardBackFaceImageSrc="/src/assets/名刺サイズの画像2.png"
+  />
   <HelloWorld />
 </template>
---
+++ src/components/BusinessCard.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const { businessCardFrontFaceImageSrc } = defineProps<{
+  businessCardFrontFaceImageSrc: string;
+  businessCardBackFaceImageSrc: string;
+}>();
+
+const isFrontFaceVisible = ref<boolean>(true);
+
+const onClickToggleFace = () => {
+  isFrontFaceVisible.value = !isFrontFaceVisible.value;
+};
+</script>
+
+<template>
+  <v-card class="business-card">
+    <v-img v-if="isFrontFaceVisible" :src="businessCardFrontFaceImageSrc" alt="Business Card Front Face Image" />
+    <v-img v-else :src="businessCardBackFaceImageSrc" alt="Business Card Back Face Image" />
+    <v-btn @click="onClickToggleFace">裏返す</v-btn>
+  </v-card>
+</template>

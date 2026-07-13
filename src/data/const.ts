4a85edee865418cbf6ca4ea757bfc05adb16c151
teakatara <teakatara@users.noreply.github.com>
2026-07-13 09:22:51 +0000

    定数定義用ファイルの追加

---
+++ src/data/const.ts
@@ -0,0 +1,29 @@
+export const P_NAME = "ティー";
+
+export const BUSINESS_CARD_LIST = [
+  {
+    note: "仮名刺1",
+    businessCardFrontFaceImageSrc: "/src/assets/名刺サイズの画像.png",
+    businessCardBackFaceImageSrc: "/src/assets/名刺サイズの画像2.png",
+  },
+  {
+    note: "仮名刺2",
+    businessCardFrontFaceImageSrc: "/src/assets/名刺サイズの画像.png",
+    businessCardBackFaceImageSrc: "/src/assets/名刺サイズの画像2.png",
+  },
+  {
+    note: "仮名刺3",
+    businessCardFrontFaceImageSrc: "/src/assets/名刺サイズの画像.png",
+    businessCardBackFaceImageSrc: "/src/assets/名刺サイズの画像2.png",
+  },
+  {
+    note: "仮名刺4",
+    businessCardFrontFaceImageSrc: "/src/assets/名刺サイズの画像.png",
+    businessCardBackFaceImageSrc: "/src/assets/名刺サイズの画像2.png",
+  },
+  {
+    note: "仮名刺5",
+    businessCardFrontFaceImageSrc: "/src/assets/名刺サイズの画像.png",
+    businessCardBackFaceImageSrc: "/src/assets/名刺サイズの画像2.png",
+  },
+];

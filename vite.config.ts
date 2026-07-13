194f48415aedcdaaad822028fa31f1953e60bbe8
teakatara <syoukatara@outlook.jp>
2026-07-13 12:18:28 +0000

    絶対パスでimportできるように設定修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import vue from "@vitejs/plugin-vue";
+import path from "path";
 import ViteFonts from "unplugin-fonts/vite";
 import { defineConfig } from "vite";
 import vuetify from "vite-plugin-vuetify";
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
   plugins: [
     vue(),
     vuetify(),

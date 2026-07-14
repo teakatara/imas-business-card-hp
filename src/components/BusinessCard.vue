f97e2ede74208d9f93b6c1ddd087880df6bf0a47
teakatara <teakatara@users.noreply.github.com>
2026-07-14 01:52:11 +0000

    BusinessCardコンポーネントのスタイルを修正し、カルーセルの名刺リストが5枚以上になるよう動的に生成するロジックを追加

--- src/components/BusinessCard.vue
+++ src/components/BusinessCard.vue
@@ -16,9 +16,9 @@ const onClickToggleFace = () => {
 </script>
 
 <template>
-  <v-card class="business-card">
+  <v-card class="business-card" width="100%" height="100%" position="relative">
     <v-img v-if="isFrontFaceVisible" :src="businessCardFrontFaceImageSrc" alt="Business Card Front Face Image" />
     <v-img v-else :src="businessCardBackFaceImageSrc" alt="Business Card Back Face Image" />
-    <v-btn @click="onClickToggleFace">裏返す</v-btn>
+    <v-btn @click="onClickToggleFace" position="absolute" style="bottom: 5px; right: 5px">裏返す</v-btn>
   </v-card>
 </template>
--- src/components/BusinessCardCarousel.vue
+++ src/components/BusinessCardCarousel.vue
@@ -1,9 +1,32 @@
 <script setup lang="ts">
+import { ref } from "vue";
+
 import "swiper/css";
 import { Swiper, SwiperSlide } from "swiper/vue";
 
 import BusinessCard from "@/components/BusinessCard.vue";
 import { BUSINESS_CARD_LIST } from "@/data/const.ts";
+
+const makeBusinessCardConfig = (businessCardList: Array<any>) => {
+  const resultConfigObj = {
+    originalBusinessCardList: businessCardList,
+    businessCardList: [...businessCardList],
+  };
+
+  if (businessCardList.length !== 0) {
+    while (resultConfigObj.businessCardList.length < 5) {
+      // 5枚未満の場合は、ループを正常動作させるために名刺を複製する
+      resultConfigObj.businessCardList.push(...businessCardList);
+    }
+  } else {
+    // TODO:名刺が1枚もない場合は、ダミーの名刺を表示するか否か検討する
+  }
+
+  return resultConfigObj;
+};
+
+const businessCardConfig = makeBusinessCardConfig(BUSINESS_CARD_LIST);
+const businessCardList = ref(businessCardConfig.businessCardList);
 </script>
 
 <template>
@@ -15,7 +38,11 @@ import { BUSINESS_CARD_LIST } from "@/data/const.ts";
     :centered-slides-bounds="true"
     :slides-per-group="1"
   >
-    <swiper-slide v-for="(businessCard, i) in BUSINESS_CARD_LIST" :key="i" style="max-width: 100%; width: 700px">
+    <swiper-slide
+      v-for="(businessCard, i) in businessCardList"
+      :key="i"
+      style="max-width: 100%; min-width: 35%; width: 700px"
+    >
       <BusinessCard
         :businessCardFrontFaceImageSrc="businessCard.businessCardFrontFaceImageSrc"
         :businessCardBackFaceImageSrc="businessCard.businessCardBackFaceImageSrc"
